b9913dc32b8ef715d421b478945e8641c2df0601
DenysenkoStas <49059676+DenysenkoStas@users.noreply.github.com>
2020-12-02 15:44:41 +0200

    Lesson 38

--- dist/js/script.js
+++ dist/js/script.js
@@ -93,7 +93,47 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
+window.addEventListener('DOMContentLoaded', () => {
+  const tabs = document.querySelectorAll('.tabheader__item'),
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
 
+  function hideTabContent() {
+    // скрытие .tabcontent
+    tabsContent.forEach(item => {
+      // item.style.display = 'none';
+      item.classList.add('hide');
+      item.classList.remove('show', 'fade');
+    }); // удаление класса у .tabheader__item
+
+    tabs.forEach(item => {
+      item.classList.remove('tabheader__item_active');
+    });
+  } // отображение .tabcontent и добавление класса у .tabheader__item
+
+
+  function showTabContent(i = 0) {
+    // tabsContent[i].style.display = 'block';
+    tabsContent[i].classList.add('show', 'fade');
+    tabsContent[i].classList.remove('hide');
+    tabs[i].classList.add('tabheader__item_active');
+  }
+
+  hideTabContent();
+  showTabContent();
+  tabsParent.addEventListener('click', event => {
+    const target = event.target;
+
+    if (target && target.classList.contains('tabheader__item')) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+});
 
 /***/ })
 
